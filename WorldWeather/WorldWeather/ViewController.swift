e2e93b3141dc3d915fa8a600b8a5971c1e7d0da8
João Soares <joao.soares@inm.pt>
2017-01-11 23:11:04 +0000

    Initial Commit

---
+++ WorldWeather/WorldWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldWeather
+//
+//  Created by João Soares on 11/01/17.
+//  Copyright © 2017 OnSoares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
